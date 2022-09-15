440493d458b716bf7c1cad0658aabbd1dbcc531b
TheChickenKnight <chcknknight@gmail.com>
2022-09-15 18:08:51 -0400

    copy

---
+++ js/dloop.js
@@ -0,0 +1,26 @@
+class Dloop {
+    constructor(genome) {
+        this.brain = genome;
+        this.brain.score = 1;
+
+        //Attributes
+        this.pos = createVector(random(width), random(height));
+        this.vel = createVector(0, 0);
+        this.accel = createVector(0, 0);
+        this.genes = {
+            speed: 0,
+            agility: 0,
+            size: 0,
+            sight: {
+                range: 0,
+                width: 0
+            }
+        };
+
+        //State
+        this.energy = 100;
+        this.health = 100;
+    }
+
+
+}
---
+++ js/main.js
@@ -0,0 +1,9 @@
+function rotate_and_draw_image(img_x, img_y, img_width, img_height, img_angle){
+    imageMode(CENTER);
+    translate(img_x+img_width/2, img_y+img_width/2);
+    rotate(PI/180*angle);
+    image(img, 0, 0, img_width, img_height);
+    rotate(-PI / 180 * img_angle);
+    translate(-(img_x+img_width/2), -(img_y+img_width/2));
+    imageMode(CORNER);
+}
